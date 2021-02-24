f72d22745b26b083f41720b7c52cfeba4ad1e17a
Maslin <maslinfar@gmail.com>
2021-02-24 13:00:11 -0800

    Flow Chart and Class Diagram(UML) has been added

--- src/components/Charts/charts.js
+++ src/components/Charts/charts.js
@@ -12,9 +12,9 @@ class Charts extends Component {
                 mermaid.parse(value);
                 output.innerHTML = '';
                 mermaid.render('theGraph', value, function (svgCode) {
-                    console.log(svgCode);
                     output.innerHTML = svgCode;
                 });
+
             } catch (err) {
                 console.error(err);
             }
@@ -22,28 +22,75 @@ class Charts extends Component {
         600,
         false
     );
-    componentDidMount() {
+
+    flowChart() {
+        var graphType = `graph TD
+        A[Christmas] -->|Get money| B(Go shopping)
+        B --> C{Let me think}
+        C -->|One| D[Laptop]
+        C -->|Two| E[iPhone]
+        C -->|Three| F[fa:fa-car Car]`;
+
+        this.componentDidMount(graphType);
+    }
+    classDiagram() {
+        var graphType = `classDiagram
+        Animal <|-- Duck
+        Animal <|-- Fish
+        Animal <|-- Zebra
+        Animal : +int age
+        Animal : +String gender
+        Animal: +isMammal()
+        Animal: +mate()
+        class Duck{
+        +String beakColor
+        +swim()
+        +quack()
+        }
+        class Fish{
+        -int sizeInFeet
+        -canEat()
+        }
+        class Zebra{
+        +bool is_wild
+        +run()
+        }`;
+        this.componentDidMount(graphType);
+    }
+    componentDidMount(graphType) {
         var output = document.getElementById('output');
         mermaid.initialize({ startOnLoad: true });
+        console.log("Working");
+        if (graphType) {
+            var graphDefinition = graphType;
+            document.getElementById("textBox").value = graphType;
+        } else {
+            var template = `graph TB
+            a-->b
+            b-->a`;
+            var graphDefinition = template;
+            document.getElementById("textBox").value = template;
+        }
 
-        var graphDefinition = `graph TB
-        a-->b
-        b-->a`;
         mermaid.render('theGraph', graphDefinition, function (svgCode) {
             console.log(svgCode);
             output.innerHTML = svgCode;
         });
     }
-    render(){
+
+    render() {
         return (
-            
+
             <div className="Charts">
+                <button onClick={(e) => this.classDiagram(e)}>flowChart</button>
+                <button onClick={(e) => this.flowChart(e)}>Class</button>
                 <textarea
+                    id="textBox"
                     rows="4"
-                    onChange={(e) => this.handleChange(e.target.value)}
+                    onChange={(e) => this.handleChange(e.target.value) }
                 />
-                <div id="output"/>
+                <div id="output" />
             </div>
         );
     }
-}export default Charts;
+} export default Charts;
