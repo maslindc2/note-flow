ed688c1617bad48889d606681fc4744db32e31ac
zach-nelson <zen123555@gmail.com>
2021-03-01 13:14:58 -0800

    implementing account page features

--- src/components/accountpage.js
+++ src/components/accountpage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+import firebase from 'firebase'
 import PasswordChangeForm from './PasswordChange';
 import { withAuthorization } from './Session';
 
@@ -8,11 +8,29 @@ import { withAuthorization } from './Session';
 
 class AccountPage extends React.Component {
   render() {
+
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+  name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+  emailVerified = user.emailVerified;
+  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                   // this value to authenticate with your backend server, if
+                   // you have one. Use User.getToken() instead.
+}
+
+
     return (
       <div>
         <h1 id={"title"}>Account Page</h1>
 
         <div>
+
+            <h1>Email:</h1>
+            <p>{email}</p>
             <h1>Change password</h1>
             <PasswordChangeForm />
         </div>
