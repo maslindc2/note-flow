84d058dc8044e09eca4dcdc372a6eb45de1d62ce
Maslin <maslinfar@gmail.com>
2021-03-03 12:09:34 -0800

    Built server file
    Project is pushed to heroku, I made a server file for it and forgot to push it so here it is woops

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server is up!');
+});
