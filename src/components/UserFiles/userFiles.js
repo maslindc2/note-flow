0208e812f0b9416c9e024b9471afa1a252ea1506
Maslin <maslinfar@gmail.com>
2021-03-03 12:42:40 -0800

    Added userFiles update from master

--- src/components/UserFiles/userFiles.js
+++ src/components/UserFiles/userFiles.js
@@ -2,40 +2,44 @@ import React from 'react'
 import './filePage.css'
 import FileCard from './fileCard'
 import user from '../UserInfo/userInfo'
-import firebase from'firebase'
+import firebase from 'firebase'
 import Firebase from '../Firebase/firebase.js';
 import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
 //What user sees as the home page of our app
-var size=0;
-function loadFIle(){
-  
+var size = 0;
+function loadFIle() {
+
   var i;
   var size;
-  var files=[];
-  var ids =[];
-  
+  var files = [];
+  var ids = [];
+
 
   //loading files information
-  for( i=0;i<user.docs_size;i++){
-    files.push(<FileCard class={"file"} key={'doc'+(i+1)} id={user.docs_ids[i]}></FileCard>);
-    console.log("test "+user.docs_ids);
-  }  
+  for (i = 0; i < user.docs_size; i++) {
+    files.push(<FileCard class={"file"} key={'doc' + (i + 1)} id={user.docs_ids[i]}></FileCard>);
+    console.log("test " + user.docs_ids);
+  }
   return files;
 }
 
 class UserFiles extends React.Component {
-  
+
   render() {
     return (
       <div class="filePageWrapper">
         <h1 id={"fileTitle"}>My Files</h1>
         <h2 id={"subtitle"}>All saved user files will be displayed here. Currently in development.</h2>
         <div class="fileDisplay">
-          
-            {loadFIle()}
+
+          {loadFIle()}
 
         </div>
       </div>
     )
   }
-}export default UserFiles
+}
+
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(UserFiles);
