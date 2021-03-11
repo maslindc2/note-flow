79ca632b4b5f7d1f61a8fbaab076db225c2a6431
Maslin <maslinfar@gmail.com>
2021-03-10 19:54:05 -0800

    Revert "Finished alignment of sign-in and sign-up"
    This reverts commit 640206a2fda72cde330d71411cf05e507c1a42f0.

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -5,7 +5,7 @@ import { withFirebase } from '../Firebase';
 import userInner from '../UserInfo/userInfo';
 
 import firebase from 'firebase';
-import ToolbarInner from '../toolbar/Toolbar';
+import ToolbarInner from'../toolbar/Toolbar';
 import $ from 'jquery'
 
 //implementation of sign in functionality
@@ -25,84 +25,86 @@ import './signInFormStyle.css'
 
 const SignIn = () => (
   <div>
-
-
-    <SignInForm />
-
-
+    
+ 
+     <SignInForm  />
+     
+ 
   </div>
 );
-
+ 
 const INITIAL_STATE = {
   email: '',
   password: '',
   error: null,
-
+  
 
 };
 
-
+ 
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
-
+ 
     this.state = { ...INITIAL_STATE };
   }
-
+  
   onSubmit = event => {
     const { email, password } = this.state;
-
+    
     this.props.firebase
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
 
-        userInner()[2]();
+          userInner()[2]();
 
         //Successful sign in will send user to editor page for now
         this.props.history.push("/homepage");
-
+        
       })
-
+      
       .catch(error => {
         this.setState({ error });
       });
-
-
+      
+ 
     event.preventDefault();
   };
-
+ 
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-
+ 
   render() {
     const { email, password, error } = this.state;
-
+ 
     const isInvalid = password === '' || email === '';
-
+ 
     return (
       <form onSubmit={this.onSubmit} class="signInFormWrapper" noValidate autoComplete="off">
         <FormControl class="formEmailPassword" variant="outlined" size="small" >
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <OutlinedInput name="email" value={email} onChange={this.onChange} label="Email" placeholder="" />
-        </FormControl>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <OutlinedInput name="email" value={email} onChange={this.onChange} label="Email" placeholder="Email Address"/>
+          </FormControl>
 
         <FormControl class="formEmailPassword" variant="outlined" size="small" >
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <OutlinedInput name="password" value={password} onChange={this.onChange} type="password" label="Password" placeholder="" />
-        </FormControl>
-
-        <Button id="signInButton" variant="outlined" disabled={isInvalid} type="submit">Sign In </Button>
-
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <OutlinedInput name="password" value={password} onChange={this.onChange} type="password" label="Password" placeholder="Password"/>
+          </FormControl>
+        
+        <Button variant="outlined" id="submitButton" disabled={isInvalid} type="submit" color={"black"}>
+          Sign In
+        </Button>
+ 
         {error && <p class="errorMessage">{error.message}</p>}
       </form>
     );
   }
-
+ 
 }
 
 const SignInForm = withRouter(withFirebase(SignInFormBase));
 
 export default SignIn;
-
+ 
 export { SignInForm };
